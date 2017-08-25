9a92ab8a6b7023dd9c2739de5dca11f8e58ef3ca
Jon Hannah <jhannah01@gmail.com>
2017-08-24 21:27:46 -0400

    Added email module with initial __hash__ logic (to be tested)

--- mailmig/models/emails.py
+++ mailmig/models/emails.py
@@ -13,5 +13,21 @@ class Email(SqlBase):
 
     imap_account = relationship('ImapAccount', backref='emails')
 
+    def _key(self):
+        if self.email:
+            ids = [k for k in self.email.keys() if k.lower() in ['message-id', 'messageid']]
+
+            if not ids:
+                return self.content
+
+            return self.email[ids[0]]
+        return self.content
+
+    def __eq__(x, y):
+        return (x._key() == y._key())
+
+    def __hash__(self):
+        return hash(self._key())
+
 
 __all__ = ['Email']
